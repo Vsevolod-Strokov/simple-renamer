318dddeaf5c2042f47f42fd90fe6e84bc8bb1294
Seva <housewhiterat@gmail.com>
2025-10-03 17:39:09 +0300

    Added user confirm to rename and folder ignore

--- renamer.py
+++ renamer.py
@@ -1,17 +1,35 @@
 import os
-path = input ('Введи путь: ')
+
+path = input('Введи путь: ')
 os.chdir(path)
 lst = os.listdir()
 dic = {}
 
+def is_folder(folder):  # - Проверка является ли объект папкой
+    for i in folder:
+        if i == '.':
+            return False
+            break
+
 for i in lst:
-    num = int(i[-6:-4])
-    dic[num] = i
+    if is_folder(i) == False:
+        num = int(i[-6:-4])
+        dic[num] = i
+    else:
+        continue
 
 pairs = dict(sorted(dic.items()))
 
-for key,value in pairs.items():
+print('\nБудут переименованы эти файлы:')
+
+for key, value in pairs.items():
     new_name = str(key) + value[-4:]
-    os.rename(value, new_name)
-    
-print ('ОК!')
+    print(f'"{value}" -> "{new_name}"')
+
+
+if  input('\nПродолжить?(Y/n): ').lower() == 'y':
+    for key, value in pairs.items():
+        new_name = str(key) + value[-4:]
+        os.rename(value, new_name)
+    print('Файлы успешно переименованы!')
+else: print("Программа завершила работу без внесения изменений")
